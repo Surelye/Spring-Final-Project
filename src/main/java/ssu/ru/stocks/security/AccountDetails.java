92e22f90f227542e6d69e0f892cf9559d5f315b9
Artyom <sgnoteii55@gmail.com>
2024-05-16 23:19:37 +0400

    feat: account details added

---
+++ src/main/java/ssu/ru/stocks/security/AccountDetails.java
@@ -0,0 +1,57 @@
+package ssu.ru.stocks.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ssu.ru.stocks.models.Account;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AccountDetails implements UserDetails {
+
+    private final Account account;
+
+    public AccountDetails(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // Нужно, чтобы получать данные аутентифицированного пользователя
+    public Account getAccount() {
+        return account;
+    }
+}
