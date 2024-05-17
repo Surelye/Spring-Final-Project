c93111979d46577d33953932a53c9b73f56040dd
Artyom <sgnoteii55@gmail.com>
2024-05-17 23:34:54 +0400

    feat: account status enum updated

--- src/main/java/ssu/ru/stocks/models/Status.java
+++ src/main/java/ssu/ru/stocks/models/Status.java
@@ -1,5 +1,25 @@
 package ssu.ru.stocks.models;
 
 public enum Status {
-    STARTER, PRO, PREMIUM
+
+    STARTER {
+        @Override
+        public String toString() {
+            return "Стартовый";
+        }
+    },
+
+    PRO {
+        @Override
+        public String toString() {
+            return "Профи";
+        }
+    },
+
+    PREMIUM {
+        @Override
+        public String toString() {
+            return "Премиум";
+        }
+    }
 }
