094e0e94880da24568168779899019af6f39db2f
Artyom <sgnoteii55@gmail.com>
2024-06-04 19:11:14 +0400

    feat: AccountStock model updated

--- src/main/java/ssu/ru/stocks/models/AccountStock.java
+++ src/main/java/ssu/ru/stocks/models/AccountStock.java
@@ -2,11 +2,15 @@ package ssu.ru.stocks.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @IdClass(AccountStockId.class)
 @Table(name = "Account_Stock")
