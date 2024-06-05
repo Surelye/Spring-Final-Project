f01c032486d501afc30276883abfb529258df069
Artyom <sgnoteii55@gmail.com>
2024-06-05 10:13:35 +0400

    feat: entry point updated

--- src/main/java/ssu/ru/stocks/StockingApplication.java
+++ src/main/java/ssu/ru/stocks/StockingApplication.java
@@ -1,13 +1,25 @@
 package ssu.ru.stocks;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Random;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockingApplication.class, args);
 	}
 
+	@Bean
+	@Scope("prototype")
+	public Random random() {
+		return new Random();
+	}
 }
