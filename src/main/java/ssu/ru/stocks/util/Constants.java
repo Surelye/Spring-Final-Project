fc74181f7e3c19fa0dfd458b4be5348589b4fd80
Artyom <sgnoteii55@gmail.com>
2024-06-05 10:12:41 +0400

    feat: status commission constants added

---
+++ src/main/java/ssu/ru/stocks/util/Constants.java
@@ -0,0 +1,11 @@
+package ssu.ru.stocks.util;
+
+public class Constants {
+
+    public static final double STARTER_LB = 4.;
+    public static final double STARTER_UB = 5.;
+    public static final double PRO_LB = 2.;
+    public static final double PRO_UB = 3.99;
+    public static final double PREMIUM_LB = 0.5;
+    public static final double PREMIUM_UB = 1.99;
+}
