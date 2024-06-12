f729ef6c3bda6ffc9d6ef22b0d40a64618354c5f
Artyom <sgnoteii55@gmail.com>
2024-06-12 13:40:00 +0400

    feat: checkEnoughBalance function updated

--- src/main/resources/static/js/checkEnoughBalance.js
+++ src/main/resources/static/js/checkEnoughBalance.js
@@ -1,16 +1,19 @@
 function checkEnoughBalance() {
-    const purchasePrice = parseFloat(document.getElementById('purchase-price-holder').value)
-    const balance = parseFloat(document.getElementById('balance-holder').value)
-    const textElement = document.getElementById('message')
+    const purchasePrice = parseFloat(document.getElementById('purchase-price-holder').value);
+    const balance = parseFloat(document.getElementById('balance-holder').value);
+    const textElement = document.getElementById('message');
 
     if (balance < purchasePrice) {
-        textElement.innerText = 'На Вашем балансе недостаточно средств для совершения операции.'
-        textElement.style.color = 'red'
+        textElement.innerText = 'На Вашем балансе недостаточно средств для совершения операции.';
+        textElement.classList.remove('hidden');
+        textElement.classList.add('show');
     } else {
-        textElement.innerText = ''
+        textElement.innerText = '';
+        textElement.classList.remove('show');
+        textElement.classList.add('hidden');
     }
 }
 
-$(document).ready(function () {
-    checkEnoughBalance()
-})
+document.addEventListener('DOMContentLoaded', function () {
+    checkEnoughBalance();
+});
